2c59601d56d20d49b012bd3a3ab2eb49d9f447d4
Jacques COUVREUR <jacques@couvreur.me>
2012-05-26 10:27:37 +0200

    Create Legomni web app bootstrap #1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Legomni::Application.config.secret_token = '09c65e8c0c9f6bf14ec491f42f2a34b49e8c9ff736084aade86ff29b60e3950cb621377afb8aa07b06b994ad5b81534c688ec1da8f6cb9a6dcbb7a1cbebd8cb3'
