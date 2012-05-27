7eaf30737d9a5f38d82d6b5fa05e99db4c9596bf
Jacques COUVREUR <jacques@couvreur.me>
2012-05-27 17:04:08 +0200

    Add one-to-many relationship from Series to Figure #3.

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,3 +1,4 @@
 class Figure < ActiveRecord::Base
   attr_accessible :creativity, :description, :name, :number, :series_id, :slogan, :speed, :strength, :url
+  belongs_to :series
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -1,3 +1,4 @@
 class Series < ActiveRecord::Base
   attr_accessible :color, :number, :url
+  has_many :figures
 end
